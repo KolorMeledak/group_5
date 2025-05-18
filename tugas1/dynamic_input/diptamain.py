a9114cba689e38326f9199b699d8f4eb09d716f8
dipta <mpradipta.arya03@gmail.com>
2025-05-18 08:05:18 +0700

    added dynamic stack input

---
+++ tugas1/dynamic_input/diptamain.py
@@ -0,0 +1,34 @@
+# - Dynamic input program with static array with stack structure, input in the form of name
+
+repeater = True
+tumpukan = ["agus","beni","budy","ianto"]
+print(*tumpukan, sep=', ')
+
+while repeater:
+    selection = input("1. Tambah Stek\n2. Kurangi Stek\n3. Lihat Paling Atas Stek\n4. Cek Stek\n5. Ukuran Stek\n0. Exit")
+
+    match selection:
+        case "1":
+            nama = input("Masukkan nama yang ingin ditambah:\n")
+            print(f"Added {nama}\n")
+            tumpukan.append(nama)
+            print(*tumpukan, sep=', ')
+        case "2":
+            if tumpukan:
+                print(f"Removed {tumpukan[-1]}")
+                tumpukan.pop(-1)
+                print(*tumpukan, sep=', ')
+            else:
+                 print("Can't remove from an empty stack")
+        case "3":
+            print(f"Top of Stack is {tumpukan[-1]}")
+        case "4":
+            if not tumpukan:
+                print("The stack is empty")
+            else:
+                print("Stack is not empty")
+        case "5":
+                print(f"Stack is {len(tumpukan)} item(s) long.")
+        case"0": 
+              repeater = False
+print("Thanks")
