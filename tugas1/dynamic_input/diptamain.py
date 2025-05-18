b61110bc747e02dad5c2d5355c4715fea0d22ea8
dipta <mpradipta.arya03@gmail.com>
2025-05-18 14:48:45 +0700

    refactored dynamic input

--- tugas1/dynamic_input/diptamain.py
+++ tugas1/dynamic_input/diptamain.py
@@ -1,32 +1,39 @@
-# - Dynamic input program with static array with stack structure, input in the form of name
-
-repeater = True
 tumpukan = ["agus","beni","budy","ianto"]
 
-while repeater:
-    print(*tumpukan, sep=', ')
-    selection = input("1. Tambah Stek\n2. Kurangi Stek\n3. Lihat Paling Atas Stek\n4. Cek Stek\n5. Ukuran Stek\n0. Exit")
+while True:
+    print(f"Current Stack: {", ".join(tumpukan)}")
+    selection = input("1. Tambah Stack\n2. Kurangi Stack\n0. Exit\n")
 
     match selection:
         case "1":
-            nama = input("Masukkan nama yang ingin ditambah:\n")
-            print(f"Added {nama}\n")
-            tumpukan.append(nama)
+            raw_nama= str(input("Masukkan nama yang ingin ditambah:\n")).split()
+            nama = ' '.join(raw_nama)
+            if nama == ('' and ' ' and '\n'):
+                print("Jangan input kosong.")
+            else:
+                print(f"Added {nama}\n")
+                tumpukan.append(nama)
         case "2":
             if tumpukan:
                 print(f"Removed {tumpukan[-1]}")
                 tumpukan.pop()
             else:
                  print("Can't remove from an empty stack")
-        case "3":
-            print(f"Top of Stack is {tumpukan[-1]}")
-        case "4":
-            if not tumpukan:
-                print("The stack is empty")
-            else:
-                print("Stack is not empty")
-        case "5":
-                print(f"Stack is {len(tumpukan)} item(s) long.")
         case"0": 
-              repeater = False
-print("Thanks")
+            break
+        case _: 
+            print("Jangan input kosong.")
+
+print(f"Final Stack: {", ".join(tumpukan)}\nThanks")
+
+    # selection = input("1. Tambah Stack\n2. Kurangi Stack\n3. Lihat Paling Atas Stack\n4. Cek Stack\n5. Ukuran Stack\n0. Exit\n")
+
+        # case "3":
+        #     print(f"Top of Stack is {tumpukan[-1]}")
+        # case "4":
+        #     if not tumpukan:
+        #         print("The stack is empty")
+        #     else:
+        #         print("Stack is not empty")
+        # case "5":
+        #         print(f"Stack is {len(tumpukan)} item(s) long.")
