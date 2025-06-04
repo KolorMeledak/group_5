8412ceb9c681d1e336ce1a39cdd37f60fe493319
KaonWry <titan2011official@gmail.com>
2025-06-04 09:45:55 +0700

    au ah

--- tugas4/bubble_sort.py
+++ tugas4/bubble_sort.py
@@ -1,4 +1,4 @@
-def bubble_sort(nums):
+def bubble_sort(nums, ascending=True):
     n = len(nums)
     print("Data awal:", nums)
     for i in range(n):
@@ -6,16 +6,30 @@ def bubble_sort(nums):
         swapped = False
         for j in range(0, n - i - 1):
             print(f"  Bandingkan {nums[j]} dan {nums[j+1]}", end="")
-            if nums[j] > nums[j + 1]:
-                nums[j], nums[j + 1] = nums[j + 1], nums[j]
-                swapped = True
-                print(f" => Tukar => {nums}")
+            if ascending:
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                    swapped = True
+                    print(f" => Tukar => {nums}")
+                else:
+                    print(" => Tidak ditukar")
             else:
-                print(" => Tidak ditukar")
+                if nums[j] < nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                    swapped = True
+                    print(f" => Tukar => {nums}")
+                else:
+                    print(" => Tidak ditukar")
         if not swapped:
             print("  Tidak ada pertukaran, data sudah terurut.")
             break
     print("\nData terurut:", nums)
 
-data = [5, 1, 4, 2, 8]
-bubble_sort(data)
+data_input = input('Tulis angka yang akan diurutkan(pisahkan dengan spasi): ').split()
+data_input = [int(i) for i in data_input if i.isdigit()]
+
+arah = input ('Urutkan dari yang terkecil ke terbesar? (y/n): ').strip().lower()
+if arah == 'y':
+    bubble_sort(data_input, True)
+else:
+    bubble_sort(data_input, False)
