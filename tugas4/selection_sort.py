f87e9c2465c215b5f4e0ec28e289adf827b4f60c
KaonWry <titan2011official@gmail.com>
2025-06-04 23:17:57 +0700

    Komplain-komplain gak enak, itu anak gua tabok! Ini video sebenarnya buat subscriber only tapi gua gak kuat pengen marah-marah. Jadi masalah makan siang bergizi gratis buat anak-anak, ada satu video yang gua lihat. Ada anak ngomong ayamnya kurang enak, kurang enak. Kurang enak pala lu PE'A kurang enak ayamnya, makan siang bergizi gratis. Anak gua, Azka, dari dulu ikut gua syuting dimana-mana yang gua kasih makan adalah makanan box yang ada disana yang buat semua orang. Dan kalau dia ngomong sama gua, "Pak, nggak enak. Aku mau yang lain." gua tabok. Gua tabok, tanya anaknya, gua tabok. Makan kamu. Ini makanan, ini sehat, semua orang makan seperti ini. Komplain-komplain gak enak, itu anak gua tabok. Makanya sampai sekarang lu tanya sama semua orang yang pernah syuting sama Azka atau dekat sama Azka. Sampai sekarang Azka kemana pun, kalau ada syuting atau ada kerjaan, Yang dicari sama dia adalah nasi box karena kebiasaan. Gak pernah komplain, enak, gak enak, dimakan sama dia. Nah, ente komplain. Sekaya apa ente? Gila! Nih mau nambahin? Kalau aku buat yang bilang nasi makan siang gratis itu nggak penting, tuh keahlian karena nggak relate. Aku dulu jajan cuma 1000 sedangkan beli makan siang itu mesti 2500 sampai 3000. Jadi yang aku bisa beli cuma Jelly Drink karena itu 750 perak harganya. Buat apa? Nunda laper, pasti tahu kalau yang relate. Jadi kalau yang bilang gak penting itu karena gak relate, mending diam Iya, yang ga enak itu bukan ayam. Maksud sekolah pagi-pagi itu ga enak.

--- tugas4/selection_sort.py
+++ tugas4/selection_sort.py
@@ -1,5 +1,8 @@
+def join_string(arr):
+    return ', '.join(map(str, arr))
+
 def selection_sort(nums, ascending=True):
-    print("Data awal:", nums)
+    print("Data awal:", join_string(nums))
     n = len(nums)
     for i in range(n):
         min_idx = i
@@ -11,13 +14,13 @@ def selection_sort(nums, ascending=True):
             else:
                 if nums[j] > nums[min_idx]:
                     min_idx = j
-        print(f"  Terkecil adalah {nums[min_idx]} di indeks {min_idx}")
+        print(f"  Terkecil adalah {nums[min_idx]} di indeks {min_idx + 1}")
         if min_idx != i:
             nums[i], nums[min_idx] = nums[min_idx], nums[i]
-            print(f"  Tukar {nums[min_idx]} dengan {nums[i]} => {nums}")
+            print(f"  Tukar {nums[min_idx]} dengan {nums[i]} => {join_string(nums)}")
         else:
             print("  Tidak perlu tukar")
-    print("\nData terurut:", nums)
+    print("\nData terurut:", join_string(nums))
 
 data_input = input('Tulis angka yang akan diurutkan(pisahkan dengan spasi): ').split()
 data_input = [int(i) for i in data_input if i.isdigit()]
